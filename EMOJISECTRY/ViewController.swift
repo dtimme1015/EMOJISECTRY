d6797559b5dd9ad9b43b672891dfdc55e45cd98f
LDENV Ruby <dietertim@mac.com>
2017-05-25 11:56:26 +0200

    Initial Commit

---
+++ EMOJISECTRY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMOJISECTRY
+//
+//  Created by Dieter Timmermann on 25.05.17.
+//  Copyright © 2017 dthcp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
